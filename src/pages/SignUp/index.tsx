b785c237b841fbf2a2b3c650cc93726d2db53ed9
Elioenay <elionay407@gmail.com>
2020-09-17 21:47:20 -0300

    validando cadastro

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
 import { FiArrowLeft, FiLock, FiMail, FiUser } from 'react-icons/fi';
 
 import { Form } from '@unform/web';
+
+import * as Yup from 'yup';
 
 import { Container, Content, Background } from './styles';
 
@@ -11,9 +13,21 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 const SignUp: React.FC = () => {
-  function handleSubmit(data: object): void {
-    console.log(data);
-  }
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+      });
+
+      await schema.validate(data, { abortEarly: false });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
 
   return (
     <Container>
